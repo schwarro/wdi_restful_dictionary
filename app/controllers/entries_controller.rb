37c1cb3ec64a3bdc1670ffdeb104bfa6086d783a
schwarro <rob.ap.schwartz@gmail.com>
2018-08-09 21:41:19 -0400

    completed exercise

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,30 +1,58 @@
 class EntriesController < ApplicationController
   def index
 # here we'll define some @instance_variables to store data from the database for the views to use
-    render :index
+    # render :index
+    @entries = Entry.all
   end
 
   def show
-    render :show
+    # render :show
+    @entry = Entry.find(params[:id])
   end
 
   def new
-    render :new
+    # render :new
+    @entry = Entry.new
   end
 
   def edit
-    render :edit
+    # render :edit
+    @entry = Entry.find(params[:id])
   end
 
   def create
-    redirect_to entries_url
+    # redirect_to entries_url
+    @entry = Entry.new
+
+    @entry.word = params[:entry][:word]
+    @entry.language = params[:entry][:language]
+    @entry.definition = params[:entry][:definition]
+
+    if @entry.save
+      redirect_to entries_url
+    else
+      render :new
+    end
   end
 
   def update
-    redirect_to entry_url(params[:id])
+    # redirect_to entry_url(params[:id])
+    @entry = Entry.find(params[:id])
+
+    @entry.word = params[:entry][:word]
+    @entry.language = params[:entry][:language]
+    @entry.definition = params[:entry][:definition]
+
+    if @entry.save
+      redirect_to entries_url
+    else
+      render :edit
+    end
   end
 
   def destroy
     redirect_to entries_url
+    @entry = Entry.find(params[:id])
+    @entry.destroy
   end
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,5 @@
 class Entry < ApplicationRecord
+  validates :word, presence: true
+  validates :language, presence: true
+  validates :definition, presence: true
 end
